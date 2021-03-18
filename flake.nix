dc3240ce0da41c05a1b7791983c969b0f9d0af4b
Gregory C. Oakes <gregcoakes@gmail.com>
2021-03-17 21:29:22 -0500

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "PXE boot a NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          rec {
+            nixosConfigurations.netboot = nixpkgs.lib.nixosSystem {
+              inherit system;
+              modules = [ nixosModule ];
+            };
+            nixosModule = { modulesPath, lib, ... }: with lib; {
+              imports = [ "${modulesPath}/installer/netboot/netboot-minimal.nix" ];
+              services.getty.autologinUser = mkForce "root";
+              systemd.services.sshd.wantedBy = mkOverride 0 [ "multi-user.target" ];
+            };
+            packages = {
+              inherit (nixosConfigurations.netboot.config.system.build)
+                netbootRamdisk
+                kernel
+                netbootIpxeScript
+                ;
+            };
+          }
+      );
+}
